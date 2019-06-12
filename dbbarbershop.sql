7c90a515368e3512ea2eb489d23080105344e731
fiorettijr <fiorettijr@gmail.com>
2019-06-12 00:53:47 -0300

    - Update Final
    - Update Final

--- dbbarbershop.sql
+++ dbbarbershop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 17-Maio-2019 às 04:25
+-- Generation Time: 12-Jun-2019 às 05:52
 -- Versão do servidor: 10.1.32-MariaDB
 -- PHP Version: 7.2.5
 
@@ -48,7 +48,20 @@ INSERT INTO `agendamento` (`id`, `dia`, `hora`, `id_usuario`, `id_barbeiro`, `id
 (6, '2019-06-07', '18:30', 46, 53, 3),
 (7, '2019-06-07', '12:30', 46, 53, 4),
 (8, '2019-06-06', '18:00', 46, 54, 1),
-(11, '2019-05-13', '12:00', 46, 53, 2);
+(11, '2019-05-13', '12:00', 46, 53, 2),
+(14, '2019-05-17', '18:00', 46, 53, 1),
+(15, '2019-05-17', '18:30', 46, 53, 4),
+(16, '2019-05-11', '18:30', 46, 53, 4),
+(17, '2019-05-02', '18:30', 46, 53, 2),
+(18, '2019-05-18', '18:00', 46, 54, 1),
+(19, '2019-05-18', '18:30', 46, 54, 4),
+(20, '2019-05-18', '14:00', 47, 54, 3),
+(21, '2019-05-18', '17:00', 47, 59, 2),
+(22, '2019-06-05', '18:30', 46, 53, 6),
+(23, '2019-06-06', '18:00', 46, 59, 6),
+(24, '2019-06-12', '08:30', 46, 54, 1),
+(25, '2019-06-20', '15:30', 46, 53, 4),
+(26, '2019-06-13', '18:30', 46, 54, 2);
 
 -- --------------------------------------------------------
 
@@ -59,19 +72,23 @@ INSERT INTO `agendamento` (`id`, `dia`, `hora`, `id_usuario`, `id_barbeiro`, `id
 CREATE TABLE `servicos` (
   `id` int(11) NOT NULL,
   `nome` varchar(200) NOT NULL,
-  `preco` int(11) NOT NULL,
-  `descricao` varchar(200) NOT NULL
+  `preco` float NOT NULL,
+  `descricao` varchar(200) NOT NULL,
+  `dt_excluir` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `servicos`
 --
 
-INSERT INTO `servicos` (`id`, `nome`, `preco`, `descricao`) VALUES
-(1, 'Corte', 25, 'Corte de cabelo tradicional'),
-(2, 'Barba', 20, 'Corte de barba tradicional'),
-(3, 'Luzes', 50, 'Deixa o cabelo com as pontas loiras'),
-(4, 'Platinar', 100, 'Deixa o cabelo platinado, patrão.');
+INSERT INTO `servicos` (`id`, `nome`, `preco`, `descricao`, `dt_excluir`) VALUES
+(1, 'Corte', 24.95, 'Corte tradicional', NULL),
+(2, 'Barba', 20, 'Corte de barba tradicional', NULL),
+(3, 'Luzes', 50, 'Deixa o cabelo com as pontas loiras', NULL),
+(4, 'Platinar', 100, 'Deixa o cabelo platinado, patrão.', '11/06/2019'),
+(5, 'Escova', 24, 'Escova no cabelo pra ficar liso e chave', NULL),
+(6, 'Bigodin', 34.25, 'FININ D+', NULL),
+(7, 'Platinar', 23.4, 'Platina show', NULL);
 
 -- --------------------------------------------------------
 
@@ -85,25 +102,31 @@ CREATE TABLE `usuarios` (
   `cpf` varchar(20) NOT NULL,
   `email` varchar(90) NOT NULL,
   `senha` varchar(50) NOT NULL,
-  `nivel` tinyint(4) NOT NULL DEFAULT '1'
+  `nivel` tinyint(4) NOT NULL DEFAULT '1',
+  `dt_excluir` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `cpf`, `email`, `senha`, `nivel`) VALUES
-(46, 'Eduardo Fioretti Junior', '44582554873', 'fiorettijr@gmail.com', 'fiorettijr', 1),
-(47, 'Pedro', '89823928', 'pedro@pedro', 'senhasenha', 1),
-(48, 'Eduardo Fioretti Junior', '4458255487', 'fiorettijr@gmail', 'asdasdasd', 1),
-(49, 'Eduardo Fioretti Junior', '123123', 'fiorettijr@g', '123123123', 1),
-(50, 'teste', '123123123', 'teste@teste', 'asdasdasd', 1),
-(51, 'teste', '232112', 'fiorettijr@teste.com', 'asdasdasd', 1),
-(52, 'Eduardo Fioretti Junior', '445825548', 'fiorettijr@gmail.c', 'asdasdasd', 1),
-(53, 'barbeiro da silva', '4444444444', 'barbeiro@barbeiro', 'barbeiro', 2),
-(54, 'barber silvester', '6456456453', 'barber@barber', 'barber', 2),
-(55, ' Adm Barber Shop', '11111111111', 'adm@adm', 'adm', 3),
-(56, 'Eduardo Fioretti Junior', '44582554874', 'fifi@fifi', 'asdasdasd', 1);
+INSERT INTO `usuarios` (`id`, `nome`, `cpf`, `email`, `senha`, `nivel`, `dt_excluir`) VALUES
+(46, 'Eduardo Fioretti Jr', '44582554873', 'fiorettijr@gmail.com', 'fiorettijr', 1, NULL),
+(47, 'Pedro Luis', '89823928', 'pedro@pedro', 'senhasenha', 1, NULL),
+(48, 'Eduardo Fioretti Junior', '4458255487', 'fiorettijr@gmail', 'asdasdasd', 1, NULL),
+(49, 'Eduardo Fioretti Junior', '123123', 'fiorettijr@g', '123123123', 1, NULL),
+(50, 'teste', '123123123', 'teste@teste', 'asdasdasd', 1, NULL),
+(51, 'teste', '232112', 'fiorettijr@teste.com', 'asdasdasd', 1, NULL),
+(52, 'Eduardo Fioretti Junior', '445825548', 'fiorettijr@gmail.c', 'asdasdasd', 1, NULL),
+(53, 'barbeiro da silva', '4444444444', 'barbeiro@barbeiro', 'barbeiro', 2, NULL),
+(54, 'barber silvester', '6456456453', 'barber@barber', 'barber', 2, NULL),
+(55, ' Adm Barber Shop', '11111111111', 'adm@adm', 'adm', 3, NULL),
+(56, 'Eduardo Fioretti Junior', '44582554874', 'fifi@fifi', 'asdasdasd', 1, NULL),
+(58, 'Eduardo Fioretti Junior', '44582554234', 'fiorettijr@gegege', 'asdasdasd', 1, NULL),
+(59, 'Jeremias Linkin Park', '44444444444', 'jeremiasbarber@gmail.com', 'jeremias', 2, '11/06/2019'),
+(60, 'Eduardo Fioretti Junior', '44582552332', 'fiorettijr@qweqwe', 'asdasdasd', 1, NULL),
+(61, 'Eduardo Fioretti Junior', '12039023922', 'fiorettijr@com', 'asdasd', 1, NULL),
+(63, 'Eduardo Fioretti Junior', '44234444873', 'fiore23ttijr@gmail.com', 'asdasd', 2, '11/06/2019');
 
 --
 -- Indexes for dumped tables
@@ -138,19 +161,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT for table `agendamento`
 --
 ALTER TABLE `agendamento`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `servicos`
 --
 ALTER TABLE `servicos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 
 --
 -- Constraints for dumped tables
